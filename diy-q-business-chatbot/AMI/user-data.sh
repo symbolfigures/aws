5613916c4a98f3f6851cfccaa964909cddcc8858
meb <meb@symbolfigures.io>
2026-03-08 11:22:27 -0700

    doc edits

--- diy-q-business-chatbot/AMI/user-data.sh
+++ diy-q-business-chatbot/AMI/user-data.sh
@@ -4,15 +4,15 @@
 aws ecr get-login-password | docker login --username AWS --password-stdin <AWS_ID>.dkr.ecr.<REGION>.amazonaws.com
 
 # pull
-docker pull <AWS_ID>.dkr.ecr.<REGION>.amazonaws.com/chatbot:latest
+docker pull <AWS_ID>.dkr.ecr.<REGION>.amazonaws.com/chatbot-rag:latest
 
 # run once to create the container with all parameters
 docker run -d \
-	--name chatbot \
+	--name chatbot-rag \
 	-p 127.0.0.1:8501:8501 \
 	--restart unless-stopped \
 	-e BUCKET_APP=<BUCKET_APP> \
 	-e BUCKET_LOG=<BUCKET_LOG> \
-	<AWS_ID>.dkr.ecr.<REGION>.amazonaws.com/chatbot:latest
+	<AWS_ID>.dkr.ecr.<REGION>.amazonaws.com/chatbot-rag:latest
 
 systemctl restart chatbot-docker
